d7ebee28d98e20318152d38f43227dd39c1817e3
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2019-07-15 01:32:27 -0700

    added new files

--- client/src/components/NewsSearch.jsx
+++ client/src/components/NewsSearch.jsx
@@ -8,25 +8,16 @@ class NewsSearch extends React.Component {
     this.changeInput = this.changeInput.bind(this)
   }
 
-<<<<<<< HEAD:client/src/components/NewsSearch.jsx
   changeInput = (e) => {
     e.preventDefault();
     this.setState({ birthday: e.target.value });
-=======
-  changeInput (event){
-    this.setState({ birthday: event.target.value });
->>>>>>> f386e715fb0474649139cd44f22f731b3d5d901c:client/src/components/search.jsx
   }
 
   render(){
     return (
       <div>
         <h4>  </h4>
-<<<<<<< HEAD:client/src/components/NewsSearch.jsx
         <input value={this.state.birthday} placeholder="YYYYMMDD" onChange={this.changeInput}/>
-=======
-        <input value={this.state.birthday} placeholder="YYYYMMDD no spaces no - no /" onChange={this.changeInput.bind(this)}/>
->>>>>>> f386e715fb0474649139cd44f22f731b3d5d901c:client/src/components/search.jsx
         <button onClick={() => (this.props.onSearch(this.state.birthday))}> Enter your birthday </button>
       </div>
     )
